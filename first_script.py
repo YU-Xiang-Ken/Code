8a74aa4761ba1dfd2cc5569bc23fd4b65994c5df
YU-Xiang-Ken <94676825+YU-Xiang-Ken@users.noreply.github.com>
2021-11-22 19:44:45 +0800

    Update first_script.py

--- first_script.py
+++ first_script.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from math import exp, log, sqrt
+
+
 print("Output #1: I'm excited to learn Python.")
 x = 4
 y = 5
@@ -21,3 +24,29 @@ print("Output #8: {0:1f}".format(y))
 r = 8/float(3)
 print("Output #9: {0:2f}".format(r))
 print("Output #10: {0:4f}".format(8.0/3))
+
+print("Output #11: {0:.4f}".format(exp(3)))
+print("Output #12: {0:.2f}".format(log(4)))
+print("Output #13: {0:.1f}".format(sqrt(81)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
