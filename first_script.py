3dc58e816f30dd6ec18e6fe2191b344b9cd35970
YU-Xiang-Ken <94676825+YU-Xiang-Ken@users.noreply.github.com>
2021-11-22 19:34:05 +0800

    Update first_script.py

--- first_script.py
+++ first_script.py
@@ -10,7 +10,14 @@ b = ["first", "second", "third", "fourth"]
 c= a+b
 print("Output #3: {0}, {1}, {2}".format(a,b,c))
 
-x=9
+x = 9
 print("Output #4: {0}".format(x))
 print("Output #5: {0}".format(3**4))
 print("Output #6: {0}".format(int(8.3)/int(2.7)))
+
+print("Output #7: {0:3f}".format(8.3/2.7))
+y = 2.5*4.8
+print("Output #8: {0:1f}".format(y))
+r = 8/float(3)
+print("Output #9: {0:2f}".format(r))
+print("Output #10: {0:4f}".format(8.0/3))
