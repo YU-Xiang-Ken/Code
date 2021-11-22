87cb3d241974452df4bbb465fafd778035430dfa
YU-Xiang-Ken <94676825+YU-Xiang-Ken@users.noreply.github.com>
2021-11-22 19:27:30 +0800

    2021/11/22

---
+++ first_script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+print("Output #1: I'm excited to learn Python.")
+x = 4
+y = 5
+z=x+y
+print("Output #2: Four plus five equals {0:d}.".format(z))
+
+a = [1, 2, 3, 4]
+b = ["first", "second", "third", "fourth"]
+c= a+b
+print("Output #3: {0}, {1}, {2}".format(a,b,c))
+
+x=9
+print("Output #4: {0}".format(x))
+print("Output #5: {0}".format(3**4))
+print("Output #6: {0}".format(int(8.3)/int(2.7)))
