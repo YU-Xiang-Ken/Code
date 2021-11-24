ef073f055ebe1003656ede9f23ef9cd0f80daecf
YU-Xiang-Ken <94676825+YU-Xiang-Ken@users.noreply.github.com>
2021-11-24 21:32:38 +0800

    Update first_script.py

--- first_script.py
+++ first_script.py
@@ -100,8 +100,18 @@ string5 = "Let's replace the spaces in this sentence with other characters."
 string5_replace = string5.replace(" ","!@!")
 print("Output #32(with !@!): {0:s}".format(string5_replace))
 string5_replace = string5.replace(" ",",")
-print("Output #32(with commas): {0:s}".format(string5_replace))
+print("Output #33(with commas): {0:s}".format(string5_replace))
 
+string6 = "Here's WHAT Happens WHEN You Use lower."
+print("Output #34: {0:s}".format(string6.lower()))
+string7 = "Here's wHAT Happens when You Use UPPER."
+print("Output #35: {0:s}".format(string7.upper()))
+string5 = "Here's WHAT Happens WHEN you use Capitalize."
+print("Output #36: {0:s}".format(string7.capitalize()))
+string5_list = string5.split()
+print("Output #37(on each word):")
+for word in string5_list:
+    print("{0:s}".format(word.capitalize()))
 
 
 
