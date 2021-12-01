a307da7c25a278f042c699046b5e698b458d1db1
YU-Xiang-Ken <94676825+YU-Xiang-Ken@users.noreply.github.com>
2021-12-01 21:08:14 +0800

    Update first_script.py

--- first_script.py
+++ first_script.py
@@ -137,5 +137,12 @@ string_to_find = r"The"
 pattern = re.compile(string_to_find, re.I)
 print("Output #40: {:s}".format(pattern.sub("a", string)))
 
+today = date.today()
+print("Output #41: today: {0!s}".format(today))
+print("Output #42: {0!s}".format(today.year))
+print("Output #43: {0!s}".format(today.month))
+print("Output #44: {0!s}".format(today.day))
+current_daytime = datetime.today()
+print("Output #45: {0!s}".format(current_daytime))
 
 
