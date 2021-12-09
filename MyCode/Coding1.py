bc1a6746fb5baac4cecfe58d93356daed7ae2243
Ken <94676825+YU-Xiang-Ken@users.noreply.github.com>
2021-12-09 20:42:54 +0800

    Update Coding1.py

--- MyCode/Coding1.py
+++ MyCode/Coding1.py
@@ -312,13 +312,16 @@ print("Output #116: {!s}".format(a_dict.get('three')))
 print("Output #117: {!s}".format(a_dict.get('four')))
 print("Output #118: {!s}".format(a_dict.get('four','Not in dict')))
 
-
-
-
-
-
-
-
+print("Output #119: {}".format(a_dict))
+dict_copy = a_dict.copy()
+ordered_dict1 = sorted(dict_copy.items(), key=lambda item: item[0])
+print("Output #120 (order by keys): {}".format(ordered_dict1))
+ordered_dict2 = sorted(dict_copy.items(), key= lambda item: item[1])
+print("Output #121 (order by value): {}".format(ordered_dict2))
+ordered_dict3 = sorted(dict_copy.items(), key=lambda x: x[1], reverse=True)
+print("Output #122 (order by values, descending): {}".format(ordered_dict3))
+ordered_dict4 = sorted(dict_copy.items(), key=lambda x: x[1], reverse=False)
+print("Output #123: (order by values, ascending): {}".format(ordered_dict4))
 
 
 
